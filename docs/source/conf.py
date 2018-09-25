8e21516f744f9a8e883428a94f5c1205022b2fc7
Sebastian Bernasek <sebastian@u.northwestern.edu>
2018-09-25 01:48:48 -0500

    built docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,7 +90,19 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo': 'logo1-01.png',
+    'logo_name': True,
+    'logo_text_align': 'left',
+
+    'sidebar_collapse': False,
+
+    'show_powered_by': False,
+
+}
+
+html_theme_options = {
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
