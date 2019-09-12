c6e0cd82ce6ef096635df7672647711c4d3c71f2
Sebastian Bernasek <sebastian@u.northwestern.edu>
2019-09-12 15:37:38 +0100

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -93,7 +93,7 @@ html_theme = 'alabaster'
 html_theme_options = {
 
     # logo
-    'logo': 'source/graphics/clones_logo.png',
+    'logo': './graphics/clones_logo.png',
     'logo_name': True,
     #'logo_text_align': 'center',
 
